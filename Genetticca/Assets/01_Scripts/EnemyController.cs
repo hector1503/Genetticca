fdcc606dd318fd20dfa6fc73eb5940d891e0c1a6
Hector <hector.castrofernandez@gmail.com>
2018-03-17 16:39:29 +0100

    Added basic enemy that divides.

---
+++ Genetticca/Assets/01_Scripts/EnemyController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+	public Transform Player;
+	public GameObject DivisionSpawn;
+	public float MoveSpeed = 1.0f;
+	public float DivisionDist = 2.0f;
+	public float StartDist = 4.0f;
+	public float StopDist = 0.5f;
+	public int MaxEnemies = 3;
+
+
+
+
+	void Start () 
+	{
+
+	}
+
+	void Update ()
+	{
+		transform.LookAt (Player);
+
+		if ((Vector3.Distance (transform.position, Player.position) < StartDist) && (Vector3.Distance (transform.position, Player.position) >= StopDist)) {
+
+			transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+
+
+
+			if (Vector3.Distance (transform.position, Player.position) <= DivisionDist) {
+				Divide ();
+			} 
+
+		}
+	}
+
+	void Divide()
+	{
+		if (GameObject.FindGameObjectsWithTag("Enemy").Length < MaxEnemies)
+		{
+			Instantiate(DivisionSpawn);
+		}
+	}	
+
+}
