d5f9860dfb56399f934888c440049fc1be985c0b
Marc Viñas Oller <marc.vinas@gmail.com>
2018-04-28 21:00:18 +0200

    Scripts de sistema de daño antiguos

---
+++ Genetticca/Assets/01_Scripts/old/enemyDamage.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyDamage : MonoBehaviour
+{
+	public float damage;
+	public float damageRate;
+	public float pushBackForce;
+
+
+
+	float currentTime;
+	//bool playerInRange;
+	GameObject thePlayer;
+	playerHealth thePlayerHealth;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+
+		currentTime = damageRate;
+		thePlayer = GameObject.FindGameObjectWithTag ("Player");
+		thePlayerHealth = thePlayer.GetComponentInChildren<playerHealth> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		//if (playerInRange && thePlayer != null) {
+			//Attack ();
+
+		//}
+
+		
+	}
+
+	//	void OnControllerColliderHit(ControllerColliderHit hit){
+	//		Rigidbody body = hit.collider.attachedRigidbody;
+	//		if (body == null || body.isKinematic)
+	//			return;
+	//		if (hit.gameObject.tag == "Player")
+	//			playerInRange = true;
+	//
+	//	}
+
+//	void OnTriggerEnter (Collider other)
+//	{
+//		if (other.tag == "Player") {
+//			playerInRange = true;
+//		}
+//	}
+//
+//	void OnTriggerExit (Collider other)
+//	{
+//		if (other.tag == "Player") {
+//			playerInRange = false;
+//		}
+//	}
+
+	void OnTriggerStay(Collider other)
+	{
+		currentTime += Time.deltaTime;
+		if (currentTime >= damageRate) {
+			
+			if (other.tag == "Player") {
+				Attack ();
+				currentTime = 0;
+				pushBack (thePlayer.transform);
+			}
+		}
+	}
+	void Attack ()
+	{
+		HealthBar.health=thePlayerHealth.addDamage (damage);
+
+	}
+
+	void pushBack (Transform pushedObject)
+	{
+		if (pushedObject != null && pushedObject.root != null) {
+			Vector3 pushDirection = new Vector3 ((pushedObject.position.x - transform.position.x), (transform.position.y - pushedObject.position.y), 0).normalized;
+			//pushDirection *= pushBackForce;
+			GameObject root = pushedObject.root.gameObject;
+			pushDirection.x *= 4;
+			pushDirection.y *= 0.5f;
+//			Rigidbody pushedRB = root.GetComponent<Rigidbody> ();
+//			pushedRB.velocity = Vector3.zero;
+//			pushedRB.AddForce (pushDirection, ForceMode.Impulse);
+
+
+			playerController myPlayerController = root.GetComponent<playerController> ();
+			myPlayerController.AddImpact (pushDirection,pushBackForce);
+		}
+
+	}
+}
---
+++ Genetticca/Assets/01_Scripts/old/playerHealth.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerHealth : MonoBehaviour
+{
+
+
+	public float fullHealth;
+	float currentHealth;
+	public GameObject deathFx;
+	GameObject thePlayer;
+	playerHealth thePlayerHealth;
+	public CheckPoint[] CheckPointsList;
+
+
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		currentHealth = fullHealth;	
+		thePlayer = GameObject.FindGameObjectWithTag ("Player");
+		thePlayerHealth = thePlayer.GetComponentInChildren<playerHealth> ();
+		//CheckPointsList = GameObject.FindGameObjectsWithTag ("CheckPoint");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	public float addDamage (float damage)
+	{
+		currentHealth -= damage;
+
+		Vector3 position = new Vector3 (transform.position.x, transform.position.y + 1, transform.position.z);
+		Instantiate (deathFx,position, Quaternion.Euler(new Vector3(-90,0,0)));
+		if (currentHealth <= 0) {
+			makeDead ();
+		}
+
+		return currentHealth;
+	}
+
+	public float addCuracion (float numCuracion)
+	{
+		currentHealth += numCuracion;
+		if (currentHealth > fullHealth)
+			currentHealth = fullHealth;
+
+		return currentHealth;
+	}
+
+	public void makeDead ()
+	{
+		Transform result;
+		result = thePlayer.transform;
+
+		if (gameObject.tag == "Player" || gameObject.tag == "PlayerChilds" || gameObject.tag == "targetPlayer") {
+			if (CheckPointsList != null) {
+				foreach (CheckPoint cp in CheckPointsList) {
+					// We search the activated checkpoint to get its position
+					if (cp.getIsActive()) {
+						result = cp.transform;
+						currentHealth = cp.getSaveHealth ();
+					}
+				}
+			}
+			
+			thePlayer.transform.position = result.position;
+			thePlayer.GetComponent<CharacterController> ().Move(Vector3.zero);
+
+		
+	
+		} else {
+			Vector3 position = new Vector3 (transform.position.x, transform.position.y + 1, transform.position.z);
+			Instantiate (deathFx, position, Quaternion.Euler (new Vector3 (-90, 0, 0)));
+			Destroy (transform.root.gameObject);
+		}
+	}
+
+	public float getPlayerHealth(){
+		return currentHealth;
+	}
+}
